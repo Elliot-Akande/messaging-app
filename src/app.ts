e00485521fb4e1e2a4d86eed6e29bd79a11c0769
Elliot Akande <elbo.lynch@gmail.com>
2024-03-09 12:32:46 +0000

    feat: add and configure passport middleware

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,11 @@ import path from "path";
 import { fileURLToPath } from "url";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import passport from "passport";
+import session from "express-session";
+import { Strategy as LocalStrategy } from "passport-local";
+import User, { UserDocument } from "./models/user.js";
+import mongoose from "mongoose";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -10,15 +15,44 @@ const __dirname = path.dirname(__filename);
 import indexRouter from "./routes/index.js";
 
 // Set up mongoose connection
-import mongoose from "mongoose";
 mongoose.set("strictQuery", false);
 
 main().catch((err) => console.error(err));
 async function main() {
-  const mongoDB = process.env.MONGO_URL;
-  if (!mongoDB) throw new Error("MONGO_URL is undefined");
-  await mongoose.connect(mongoDB);
+  await mongoose.connect(process.env.MONGO_URL);
 }
+
+// Configure passport
+passport.use(
+  new LocalStrategy(async (username: string, password: string, done) => {
+    try {
+      const user = await User.findOne({ username }).exec();
+      if (!user) return done(null, false);
+      if (!user.validatePassword(password)) return done(null, false);
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+
+passport.serializeUser((user, done) => {
+  const isUserDocument = (user: object): user is UserDocument => {
+    return "username" in user && "password" in user;
+  };
+
+  if (isUserDocument(user)) done(null, user.id);
+  return done(new Error("invalid user object"));
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id).exec();
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
 
 const app = express();
 
@@ -27,6 +61,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use("/", indexRouter);
 
