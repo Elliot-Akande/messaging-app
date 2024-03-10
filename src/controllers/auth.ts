8670eecb11cdfd91dfe019cc727c09ae055436d9
Elliot Akande <elbo.lynch@gmail.com>
2024-03-10 13:19:03 +0000

    feat: add input validation & success msg to login

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,3 +1,25 @@
+import { JSONSchemaType } from "ajv";
+import { NextFunction, Request, Response } from "express";
 import passport from "passport";
+import validateBody from "../middleware/validateBody.js";
+import User, { UserInput } from "../models/user.js";
 
-export const login = passport.authenticate("local");
+// Used for req body validation with ajv
+const userSchema: JSONSchemaType<UserInput> = {
+  type: "object",
+  properties: {
+    username: { type: "string" },
+    password: { type: "string" },
+  },
+  required: ["username", "password"],
+  additionalProperties: false,
+};
+
+export const login = [
+  validateBody(userSchema),
+  passport.authenticate("local"),
+  (req: Request, res: Response, next: NextFunction) => {
+    res.json({ msg: "Logged in successfully." });
+  },
+];
+
