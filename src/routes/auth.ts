0453d8de93066f645ad596e8d2821c1db32a9ef4
Elliot Akande <elbo.lynch@gmail.com>
2024-03-10 13:23:24 +0000

    feat: add signup route

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { login } from "../controllers/auth.js";
+import { login, signup } from "../controllers/auth.js";
 
 const router = express.Router();
 
 // POST login request.
 router.post("/login", login);
+
+// POST signup request.
+router.post("/signup", signup);
 
 export default router;
