c7bb4f8951257a1686daa1a1ed7d8760643449e2
Elliot Akande <elbo.lynch@gmail.com>
2024-03-13 12:11:37 +0000

    feat: create chats POST route controller

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -1,8 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import Chat from "../models/Chat.js";
 import asyncHandler from "express-async-handler";
+import { JSONSchemaType } from "ajv";
+import validateBody from "../middleware/validateBody.js";
 import User, { UserDocument } from "../models/User.js";
+import createHttpError from "http-errors";
 
+interface UserIDs {
+  users: string[];
+}
+
+const userSchema: JSONSchemaType<UserIDs> = {
+  type: "object",
+  properties: {
+    users: { type: "array", items: { type: "string" } },
+  },
+  required: ["users"],
+  additionalProperties: false,
+};
 
 export const chatsGET = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -21,3 +36,25 @@ export const chatsGET = asyncHandler(
   }
 );
 
+export const chatsPOST = [
+  validateBody(userSchema),
+  asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const userIDs = req.body.users;
+
+    const users = await Promise.all<UserDocument>(
+      userIDs.map(async (id: string): Promise<UserDocument> => {
+        const user = await User.findById(id).exec();
+        if (!user) throw createHttpError(404, `User with ID ${id} not found`);
+        return user;
+      })
+    );
+
+    // User is authenticated in app.ts
+    // Passport deserializes ID to UserDocument
+    users.push(req.user as UserDocument);
+
+    const chat = await Chat.create({ users });
+
+    res.status(201).json({ data: chat.id });
+  }),
+];
