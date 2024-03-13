40e6c2f8942701d8cfd2d8ebb1848e9057a31ec4
Elliot Akande <elbo.lynch@gmail.com>
2024-03-13 13:59:54 +0000

    feat: add message creation method

--- src/models/Chat.ts
+++ src/models/Chat.ts
@@ -1,5 +1,5 @@
 import mongoose, { ObjectId } from "mongoose";
-import { MessageDocument, messageSchema } from "./Message.js";
+import Message, { MessageDocument, messageSchema } from "./Message.js";
 
 export interface ChatInput {
   users: ObjectId[];
@@ -7,11 +7,27 @@ export interface ChatInput {
 
 export interface ChatDocument extends ChatInput, mongoose.Document {
   messages: MessageDocument[];
+  createMessage: (message: string, author: ObjectId) => Promise<string>;
 }
 
 const chatSchema = new mongoose.Schema({
   users: [{ type: mongoose.Types.ObjectId, ref: "User", required: true }],
   messages: [messageSchema],
 });
+
+chatSchema.methods.createMessage = async function (
+  content: string,
+  author: ObjectId
+): Promise<MessageDocument> {
+  const chat = this as ChatDocument;
+  const message = new Message({
+    content,
+    author,
+  });
+  chat.messages.push(message);
+  await chat.save();
+
+  return message;
+};
 
 export default mongoose.model<ChatDocument>("Chat", chatSchema);
