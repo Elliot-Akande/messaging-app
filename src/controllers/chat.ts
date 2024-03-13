d23d38fa7f886b3aa9fd560ad6c2b55da7abbf7f
Elliot Akande <elbo.lynch@gmail.com>
2024-03-13 12:11:02 +0000

    fix: properly filter data in chats GET controller

--- src/controllers/chat.ts
+++ src/controllers/chat.ts
@@ -1,11 +1,23 @@
 import { NextFunction, Request, Response } from "express";
 import Chat from "../models/Chat.js";
 import asyncHandler from "express-async-handler";
+import User, { UserDocument } from "../models/User.js";
+
 
 export const chatsGET = asyncHandler(
   async (req: Request, res: Response, next: NextFunction) => {
-    const userID = req.user;
-    const chats = Chat.find({ users: userID });
+    const user = req.user as UserDocument;
+
+    // Get all chats user is part of
+    // Exclude logged in user from returned data
+    const chats = await Chat.find({ users: user.id }, { users: 1 })
+      .populate({
+        path: "users",
+        match: { _id: { $ne: user.id } },
+        select: "username",
+      })
+      .exec();
     res.json({ data: chats });
   }
 );
+
