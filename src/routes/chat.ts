6a92e72d20fc7c8695e009e6e0031431edf42758
Elliot Akande <elbo.lynch@gmail.com>
2024-03-13 14:57:05 +0000

    feat: add chat data GET route

--- src/routes/chat.ts
+++ src/routes/chat.ts
@@ -2,11 +2,13 @@ import express from "express";
 import {
   createChat,
   createMessage,
+  getChat,
   getUserChats,
 } from "../controllers/chat.js";
 
 const router = express.Router();
 
+router.get("/:chatId", getChat);
 router.get("/", getUserChats);
 
 router.post("/:chatId", createMessage);
